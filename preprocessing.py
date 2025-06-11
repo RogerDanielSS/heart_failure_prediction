ed5bc33409040cb0c797384eb0c26d08ec10326f
rogerdanielss <danielsimoesp@outlook.com>
2025-06-11 09:20:07 -0300

    feat: remove zero cholesterol rows

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,15 @@ from training import training_menu
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import os
 
+def remove_zero_cholesterol_rows(df_processed):
+    # Filtrar linhas onde Colesterol != 0
+    df_processed = df_processed[df_processed['Colesterol'] != 0]
+    
+    # Resetar índice se necessário
+    df_processed.reset_index(drop=True, inplace=True)
+    
+    return df_processed
+
 def normalize_numeric_columns(df_processed):
     numeric_features = ['Idade', 'PressaoArterialRepouso', 'Colesterol', 
                        'FrequenciaCardiacaMaxima', 'DepressaoSegST']
@@ -29,9 +38,25 @@ def create_indexes_for_categorical_columns(df_processed):
 
     return df_processed
 
-def default_preprocess(df):
+def little_basic_preprocess(df):
     df_processed = df.copy()
     
+    df_processed = create_indexes_for_categorical_columns(df_processed)
+    
+    return df_processed
+
+def basic_preprocess(df):
+    df_processed = df.copy()
+    
+    df_processed = normalize_numeric_columns(df_processed)
+    df_processed = create_indexes_for_categorical_columns(df_processed)
+    
+    return df_processed
+
+def intermediary_A_preprocess(df):
+    df_processed = df.copy()
+    
+    df_processed = remove_zero_cholesterol_rows(df_processed)
     df_processed = normalize_numeric_columns(df_processed)
     df_processed = create_indexes_for_categorical_columns(df_processed)
     
@@ -42,18 +67,23 @@ def preprocessing_menu(df):
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')  # Clears terminal
         print("\n=== Pré processamento ===")
-        print("1 - Padrão")
+        print("1 - Basiquinho: \n-> Indexa variáveis categóricas")
+        print("\n2 - Básico:  \n-> Indexa variáveis categóricas \n-> Normaliza variáveis numéricas")
+        print("\n3 - Intermediário A (Recomendado):  \n-> Exclui linhas que contém colesterol == 0 \n-> Indexa variáveis categóricas \n-> Normaliza variáveis numéricas")
+        print("\n4 - Voltar")
         
         choice = input("Escolha uma opção: ")
         
         if choice == '1':
-            df_processed = default_preprocess(df)
+            df_processed = little_basic_preprocess(df)
             redirect_to_training_menu(df_processed)
-        # elif choice == '2':
-        #     analyze_outliers(df)
-        # elif choice == '3':
-        #     analyze_distributions(df)
-        # elif choice == '4':
+        if choice == '2':
+            df_processed = basic_preprocess(df)
+            redirect_to_training_menu(df_processed)
+        if choice == '3':
+            df_processed = intermediary_A_preprocess(df)
+            redirect_to_training_menu(df_processed)
+        elif choice == '4':
             break
         else:
             print("Opção inválida. Tente novamente.")
